be05f7ab5ad1d9cc3bc709787266a1645eaccf93
Alan <linpd2@gmail.com>
2014-03-01 12:54:09 -0500

    [minor] comments

--- Answer.js
+++ Answer.js
@@ -1,6 +1,6 @@
 "use strict";
 
-(function(window) {
+(function(window, undefined) {
 
   /**
    * Initialize `Selection` with given root node for traversing
@@ -15,12 +15,6 @@
     this.currResults;
     this.visited = [];
     this.resultSet = [[root]];
-  };
-
-  Selection.AttrType = {
-    ID: "id",
-    CLASS: "class",
-    TAG: "tag"
   };
 
   var selection;
@@ -75,19 +69,19 @@
 
     setPredicate: function(type, value) {
       switch (type) {
-        case Selection.AttrType.ID:
+        case "id":
           this.predicate = function(obj) {
             if (!obj.id) return false;
             return obj.id === value;
           };
         break;
-        case Selection.AttrType.CLASS:
+        case "class":
           this.predicate = function(obj) {
             if (!obj.classList) return false;
             return obj.classList.contains(value);
           };
         break;
-        case Selection.AttrType.TAG:
+        case "tag":
           this.predicate = function(obj) {
             if (!obj.tagName) return false;
             return obj.tagName.toLowerCase() === value.toLowerCase();
@@ -174,162 +168,278 @@
     },
 
     getElementsById: function(value) {
-      this.getElements(Selection.AttrType.ID, value);
+      this.getElements("id", value);
     },
 
     getElementsByClassName: function(value) {
-      this.getElements(Selection.AttrType.CLASS, value);
+      this.getElements("class", value);
     },
 
     getElementsByTagName: function(value) {
-      this.getElements(Selection.AttrType.TAG, value);
+      this.getElements("tag", value);
     }
   };
 
-  var tokens = [];
+  /**
+   * Initialize `Lexer` with token list
+   */
 
-  var getTok = function() {
-    return tokens.shift();
-  };
+  var Lexer = function() {
+    this.tokens = [];
+  }
 
-  var peekTok = function() {
-    return tokens[0];
-  };
+  /**
+   * Lexer prototype
+   */
 
-  var TokenType = {
-    IDENT: "ident",
-    SPACE: "space",
-    HASH:  "hash",
-    CLASS: "class",
-    COMMA: "comma"
-  };
+  Lexer.prototype = {
 
-  var Token = function(name, value) {
-    this.name = name;
-    switch (name) {
-      case TokenType.SPACE:
-        this.value = " ";
-      break;
-      case TokenType.HASH:
-        this.value = value.slice(1);
-      break;
-      case TokenType.CLASS:
-        this.value = value.slice(1);
-      break;
-      default:
-        this.value = value;
-      break;
-    }
-  };
+    /**
+     * Save original constructor
+     */
 
-  var TokenPattern = (function() {
-    // Basic patterns
-    var nmstart = "[_a-z]";
-    var nmchar = "[_a-z0-9-]";
-    var name = nmchar + "+";
-    var ident = "[-]?" + nmstart + nmchar + "*";
-    var space = "[ \\t\\r\\n\\f]+";
-    var w = "[ \\t\\r\\n\\f]*";
-    // Token patterns
-    var patterns = {};
-    patterns[TokenType.IDENT] = new RegExp("^" + ident),
-    patterns[TokenType.SPACE] = new RegExp("^" + space),
-    patterns[TokenType.HASH] = new RegExp("^#" + name),
-    patterns[TokenType.CLASS] = new RegExp("^\\." + name),
-    patterns[TokenType.COMMA] = new RegExp("^" + w + "\\,")
-    return patterns;
-  })();
+    constructor: Lexer,
 
-  var tokenize = function(str) {
-    var tokens = [];
-    while (str.length) {
-      var hasSelection = false;
-      for (var k in TokenType) {
-        var tokName = TokenType[k];
-        var match = str.match(TokenPattern[tokName]);
-        if (match) {
-          hasSelection = true;
-          var val = match[0];
-          tokens.push(new Token(tokName, val));
-          str = str.slice(val.length);
-          break;
+    TokenTypes: [
+      "ident",
+      "space",
+      "hash",
+      "class",
+      "comma"
+    ],
+
+    /**
+     * Create a new token
+     *
+     * @param {String} type
+     * @param {String} value
+     */
+
+    newTok: function(type, value) {
+      switch (type) {
+        case "space":
+          value = " ";
+        break;
+        case "hash":
+          // Ignore hash
+          value = value.slice(1);
+        break;
+        case "class":
+          // Ignore period
+          value = value.slice(1);
+        break;
+        default:
+          value = value;
+        break;
+      }
+      return {
+        type: type,
+        value: value
+      };
+    },
+
+    /**
+     * Set up token patterns for matching
+     */
+
+    TokenPattern: (function() {
+      // Basic patterns
+      var nmstart = "[_a-z]";
+      var nmchar = "[_a-z0-9-]";
+      var name = nmchar + "+";
+      var ident = "[-]?" + nmstart + nmchar + "*";
+      var space = "[ \\t\\r\\n\\f]+";
+      var w = "[ \\t\\r\\n\\f]*";
+      // Token patterns
+      var patterns = {};
+      patterns["ident"] = new RegExp("^" + ident),
+      patterns["space"] = new RegExp("^" + space),
+      patterns["hash"] = new RegExp("^#" + name),
+      patterns["class"] = new RegExp("^\\." + name),
+      patterns["comma"] = new RegExp("^" + w + "\\,")
+      return patterns;
+    })(),
+
+    /**
+     * Split input string into tokens
+     *
+     * @param {String} str
+     */
+
+    tokenize: function(str) {
+      this.tokens = [];
+      while (str.length) {
+        var hasSelection = false;
+        for (var i in this.TokenTypes) {
+          var tokType = this.TokenTypes[i];
+          var match = str.match(this.TokenPattern[tokType]);
+          if (match) {
+            hasSelection = true;
+            var val = match[0];
+            this.tokens.push(this.newTok(tokType, val));
+            // Consume tok
+            str = str.slice(val.length);
+            break;
+          }
         }
-      }
 
-      if (!hasSelection) break;
+        if (!hasSelection) break;
+      }
+      return this.tokens;
     }
-    return tokens;
+  };
+
+  /**
+   * Initialize `Parser` with lexer
+   */
+
+  var Parser = function() {
+    this.lexer = new Lexer();
+    this.selection;
+    this.tokens = [];
   }
 
-  var selectorGroup = function(tok) {
-    selector(tok);
-    tok = getTok();
-    while (tok && tok.name === TokenType.COMMA) {
-      selection.newSelection();
-      tok = getTok();
-      if (tok.name === TokenType.SPACE) {
-        selector(getTok());
-      } else {
-        selector(tok);
+  /**
+   * Parser prototype
+   */
+
+  Parser.prototype = {
+
+    /**
+     * Save original constructor
+     */
+
+    constructor: Parser,
+
+    /**
+     * Consumes a token
+     *
+     * @return {Object}
+     */
+
+    eatTok: function() {
+      return this.tokens.shift();
+    },
+
+    /**
+     * Lookahead one token
+     *
+     * @return {Object}
+     */
+
+    peekTok: function() {
+      return this.tokens[0];
+    },
+
+    /**
+     * selectors_group
+     * : selector [ COMMA S* selector ]*
+     */
+
+    selectorGroup: function(tok) {
+      this.selector(tok);
+      tok = this.eatTok();
+      while (tok && tok.type === 'comma') {
+        this.selection.newSelection();
+        tok = this.eatTok();
+        if (tok && tok.type === 'space') {
+          this.selector(this.eatTok());
+        } else {
+          this.selector(tok);
+        }
+        tok = this.eatTok();
       }
-      tok = getTok();
-    }
-  };
+    },
 
-  var selector = function(tok) {
-    selection.setDescendentFilter();
-    simpleSelectorSequence(tok);
-    while (combinator(peekTok())
-        && getTok()
-        && simpleSelectorSequence(peekTok())
-        && getTok());
-  };
+    /**
+     * selector
+     * : simple_selector_sequence [ combinator simple_selector_sequence ]*
+     */
 
-  var simpleSelectorSequence = function(tok) {
-    if (typeSelector(tok) || specialSelector(tok)) {
-      selection.setCurrentFilter();
-      while (specialSelector(peekTok()) && getTok());
-    }
-  };
+    selector: function(tok) {
+      this.selection.setDescendentFilter();
+      this.simpleSelectorSequence(tok);
+      while (this.combinator(this.peekTok())
+          && this.eatTok()
+          && this.simpleSelectorSequence(this.peekTok())
+          && this.eatTok());
+    },
 
-  var typeSelector = function(tok) {
-    if (!tok) return;
-    if (tok.name === TokenType.IDENT) {
-      selection.getElementsByTagName(tok.value);
-      return tok;
-    }
-  };
+    /**
+     * simple_selector_sequence
+     * : [ type_selector ]
+     *   [ special_selector ]*
+     * | [ special_selector ]+
+     */
 
-  var specialSelector = function(tok) {
-    if (!tok) return;
-    if (tok.name === TokenType.HASH) {
-      selection.getElementsById(tok.value);
-      return tok;
-    } else if (tok.name === TokenType.CLASS) {
-      selection.getElementsByClassName(tok.value);
-      return tok;
-    }
-  };
+    simpleSelectorSequence: function(tok) {
+      if (this.typeSelector(tok) || this.specialSelector(tok)) {
+        this.selection.setCurrentFilter();
+        while (this.specialSelector(this.peekTok()) && this.eatTok());
+      }
+    },
 
-  var combinator = function(tok) {
-    if (!tok) return;
-    if (tok.name === TokenType.SPACE) {
-      selection.setDescendentFilter();
-      return tok;
-    }
-  };
+    /**
+     * type_selector
+     * : IDENT
+     */
 
-  window.Parser = {
-    $: function(str) {
-      selection = new Selection(document);
-      tokens = tokenize(str);
-      selectorGroup(getTok());
-      return selection.getResults();
+    typeSelector: function(tok) {
+      if (!tok) return;
+      if (tok.type === "ident") {
+        this.selection.getElementsByTagName(tok.value);
+        return tok;
+      }
+    },
+
+    /**
+     * special_selector
+     * : [ HASH | class ]+
+     */
+
+    specialSelector: function(tok) {
+      if (!tok) return;
+      if (tok.type === "hash") {
+        this.selection.getElementsById(tok.value);
+        return tok;
+      } else if (tok.type === "class") {
+        this.selection.getElementsByClassName(tok.value);
+        return tok;
+      }
+    },
+
+    /**
+     * combinator
+     * : S+
+     */
+
+    combinator: function(tok) {
+      if (!tok) return;
+      if (tok.type === "space") {
+        this.selection.setDescendentFilter();
+        return tok;
+      }
+    },
+
+    /**
+     * Parse the selector input
+     *
+     * @param {String} input
+     */
+
+    parse: function(input) {
+      this.selection = new Selection(document);
+      this.tokens = this.lexer.tokenize(input);
+      this.selectorGroup(this.eatTok());
+      return this.selection.getResults();
     }
   }
+
+  window.Parser = Parser;
 })(window);
 
+var parser = new Parser();
 var $ = function (selector) {
-  return Parser.$(selector);
+  return parser.parse(selector);
 }
 
